ad609038a4481a16a4b49957d7a9d3c140ed6305
anna128999 <annakosaruk270@gmail.com>
2026-05-15 23:30:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = document.getElementById('title');
+const mybtn = document.getElementById('mybtn');
+console.log('Знайшов заголовок:', title); 
+console.log('Знайшов кнопку:', mybtn);
+mybtn.addEventListener('click', function() { 
+  title.textContent = 'JS працює! 🎉'; 
+  title.style.color = 'teal';
+   mybtn.textContent = 'Натиснуто ✓'; 
+  mybtn.style.background = 'teal';   
+mybtn.style.color = 'white'; });
